6291a41833e06b164d0d7c6771f99f48a6b1a23a
BautistaFernandezGramajo <106882793+BautistaFernandezGramajo@users.noreply.github.com>
2022-08-28 16:27:52 -0300

    Update Ejercicio3.java

--- src/tp/matrices/Ejercicio3.java
+++ src/tp/matrices/Ejercicio3.java
@@ -15,6 +15,6 @@ public class Ejercicio3 {
      */
     public static void main(String args[]) {
         // TODO code application logic here
-        System.out.println("");
+        System.out.println();
     }
 }
