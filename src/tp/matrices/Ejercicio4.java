6b7812f3672f8fb7eda208535643d9531090a670
BautistaFernandezGramajo <bautista.fernandez@est.fi.uncoma.edu.ar>
2022-08-28 17:58:57 -0300

    avance ejercicio 4

--- src/tp/matrices/Ejercicio4.java
+++ src/tp/matrices/Ejercicio4.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class Ejercicio4 {
 
     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         // Ejercicio A
-        int[][] matriz = new int[10][6];
+        int[][] matriz = new int[2][6];
         cargarNotaAlumnos(matriz);
-        promedioAlumnos(matriz);
+        // promedioAlumnos(matriz); // Punto A
         // Ejercicio B
+        int nroAlum;
         System.out.println("Ingrese el numero de alumno para visualizar sus notas y su promedio.");
-
+        nroAlum = sc.nextInt();
+        promedioAlumnoIndividual(matriz, nroAlum);
     }
 
     public static void cargarNotaAlumnos(int[][] mat) {
@@ -44,5 +47,21 @@ public class Ejercicio4 {
             System.out.print("El promedio del alumno " + (fil + 1) + " es " + promedio);
             System.out.println("");
         }
+    }
+
+    public static void promedioAlumnoIndividual(int[][] mat, int nroAlumno) {
+        nroAlumno = nroAlumno - 1; // Ya que las filas de la matriz comienzan en 0, debo restarle uno para que no salga de rango
+        int col;
+        double promedio = 0;
+        int cantCol;
+        cantCol = mat[0].length;
+        for (col = 0; col < cantCol; col++) {
+            System.out.print(mat[nroAlumno][col] + " ");
+            promedio = promedio + mat[nroAlumno][col];
+        }
+        promedio = promedio / 6;
+        System.out.println("");
+        System.out.print("El promedio del alumno " + nroAlumno + " es " + promedio);
+
     }
 }
