1bd9b19c70f3529352432d5e7b04ab107078aefc
BautistaFernandezGramajo <bautista.fernandez@est.fi.uncoma.edu.ar>
2022-08-28 17:40:47 -0300

    avance ejercicio 4

--- src/tp/matrices/Ejercicio4.java
+++ src/tp/matrices/Ejercicio4.java
@@ -5,8 +5,44 @@ import java.util.Scanner;
 public class Ejercicio4 {
 
     public static void main(String args[]) {
+        // Ejercicio A
         int[][] matriz = new int[10][6];
-        Matriz.cargarNotaAlumnos(matriz);
-        Matriz.promedioAlumnos(matriz);
+        cargarNotaAlumnos(matriz);
+        promedioAlumnos(matriz);
+        // Ejercicio B
+        System.out.println("Ingrese el numero de alumno para visualizar sus notas y su promedio.");
+
+    }
+
+    public static void cargarNotaAlumnos(int[][] mat) {
+        Scanner sc = new Scanner(System.in);
+        int fil, col;
+        int cantFil, cantCol, valorLeido;
+        cantFil = mat.length;
+        cantCol = mat[0].length;
+        for (fil = 0; fil < cantFil; fil++) {
+            for (col = 0; col < cantCol; col++) {
+                System.out.print("Ingrese la nota de alumno " + (fil + 1) + ", examen " + (col + 1) + " : ");
+                valorLeido = sc.nextInt();
+                mat[fil][col] = valorLeido;
+                System.out.println("");
+            }
+        }
+    }
+
+    public static void promedioAlumnos(int[][] mat) {
+        int fil, col;
+        double promedio = 0;
+        int cantFil, cantCol;
+        cantFil = mat.length;
+        cantCol = mat[0].length;
+        for (fil = 0; fil < cantFil; fil++) {
+            for (col = 0; col < cantCol; col++) {
+                promedio = promedio + mat[fil][col];
+            }
+            promedio = promedio / 6;
+            System.out.print("El promedio del alumno " + (fil + 1) + " es " + promedio);
+            System.out.println("");
+        }
     }
 }
