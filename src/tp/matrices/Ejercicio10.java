64bd5e18a52782a43e5be9771ee5a17c19e34e22
BautistaFernandezGramajo <bautista.fernandez@est.fi.uncoma.edu.ar>
2022-08-31 15:15:22 -0300

    avance ejercicio 10 pseudo y java un poco

--- src/tp/matrices/Ejercicio10.java
+++ src/tp/matrices/Ejercicio10.java
@@ -1,4 +1,3 @@
-
 package tp.matrices;
 
 import java.util.Scanner;
@@ -10,29 +9,39 @@ import java.util.Scanner;
 public class Ejercicio10 {
 
     public static void main(String args[]) {
+        // Consigna
+        /* A partir de una matriz numérica se desea generar un arreglo que contenga todos los elementos de una
+         columna dada. Diseñe un algoritmo que solicite al usuario las dimensiones de la matriz, la cargue, luego 
+         le solicite la columna con la cual desea cargar el arreglo, y una vez que lo cargue lo muestre por pantalla.*/
+
         Scanner sc = new Scanner(System.in);
         int cantF, cantC, columnaS;
         System.out.print("Ingrese cant F. :");
         cantF = sc.nextInt();
         System.out.print("Ingrese cant C. :");
         cantC = sc.nextInt();
+        // Defino y creo la matriz inicial según los parametros
         int[][] matriz = new int[cantF][cantC];
-        Matriz.cargarInt(matriz);
+        cargarInt(matriz);
         System.out.print("Ingrese la columna que desea :");
         columnaS = sc.nextInt();
+        // Defino el arreglo donde se guardaran los elementos
         int[] arregloCol;
-        arregloCol = guardarColumna(matriz, columnaS); // falta modularizar
+        arregloCol = guardarColumna(matriz, columnaS); // Le asigno el arreglo creado en el modulo
         System.out.println("El nuevo arreglo es: ");
         imprimirArreglo(arregloCol);
     }
-    
-    public static void imprimirArreglo(int[]arr){
+
+    public static void imprimirArreglo(int[] arr) {
+        // Dado un arreglo, imprime sus elementos
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]+ " ");
+            System.out.println(arr[i] + " ");
         }
     }
-    
+
     public static int[] guardarColumna(int[][] mat, int columnaS) {
+        // Dada una matriz y un valor entero que represente el valor de una columna,
+        // devuelve un arreglo que contiene los elementos de la misma.
         int[] arr;
         int fil;
         int cantFil;
@@ -40,10 +49,24 @@ public class Ejercicio10 {
         arr = new int[cantFil]; // Ya que la cantidad de elementos que tiene una columna esta det. por las filas...
         for (fil = 0; fil < cantFil; fil++) {
             // Las filas van cambiando pero las columnas no.
-            arr[fil] = mat[fil][columnaS-1]; 
+            arr[fil] = mat[fil][columnaS - 1];
         }
         return arr;
     }
     
-    
+        public static void cargarInt(int[][] mat) {
+        Scanner sc = new Scanner(System.in);
+        int fil, col;
+        int cantFil, cantCol, valorLeido;
+        cantFil = mat.length;
+        cantCol = mat[0].length;
+        for (fil = 0; fil < cantFil; fil++) {
+            for (col = 0; col < cantCol; col++) {
+                System.out.println("Ingrese el valor en la fila " + (fil + 1) + " y la columna " + (col + 1));
+                valorLeido = sc.nextInt();
+                mat[fil][col] = valorLeido;
+            }
+        }
+    }
+
 }
